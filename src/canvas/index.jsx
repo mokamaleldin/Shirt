cb8ac3850174736478c040125dd82387a9097b38
mohamedkeliwa <mhmdalknj496@gmail.com>
2023-09-28 14:41:04 +0300

    first commit

---
+++ src/canvas/index.jsx
@@ -0,0 +1,30 @@
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center } from '@react-three/drei';
+
+import Shirt from './Shirt';
+import Backdrop from './Backdrop';
+import CameraRig from './CameraRig';
+
+
+const canvasModel = () => {
+  return (
+    <Canvas
+        shadows
+        camera={{position:[0,0,0],fov: 30}}
+        gl={{preserveDrawingBuffer:true}}
+        className='w-full max-w-full h-full translate-all ease-in'
+    >
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+
+      <CameraRig>
+        <Backdrop />
+        <Center>
+          <Shirt />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  )
+}
+
+export default canvasModel;
